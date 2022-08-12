35a2978452917f338a2398d04e1e685777021911
revazashvili <levanrevazashvili@gmail.com>
2022-08-12 21:51:29 +0400

    add weathers endpoint as minimal api endpoint

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -10,4 +10,24 @@ app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weathers", () => Enumerable.Range(1, 5).Select(index =>
+            new WeatherForecast
+            (
+                DateTime.Now.AddDays(index),
+                Random.Shared.Next(-20, 55),
+                summaries[Random.Shared.Next(summaries.Length)]
+            ))
+        .ToArray())
+    .WithName("GetWeatherForecast");
+
 app.Run();
+
+internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
